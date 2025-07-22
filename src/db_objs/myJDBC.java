0080b5592cae3db1899636feec5a5ea7e7559efb
Farid Izzat <i.faridizzat@gmail.com>
2025-07-22 22:05:12 +0800

    setup DB config

---
+++ src/db_objs/myJDBC.java
@@ -0,0 +1,63 @@
+package db_objs;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.math.BigDecimal;
+import java.sql.*;
+
+public class myJDBC {
+
+
+    //database configuration
+    //get from ENV
+    private static final Dotenv dotenv = Dotenv.load();
+
+    private static final String DB_URL = dotenv.get("DB_URL");
+    private static final String DB_USERNAME = dotenv.get("DB_USERNAME");
+    private static final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
+
+    //if valid return an object with the user's information
+    public static User validateLogin(String username, String password) {
+        try {
+            //establish a connection to the database using configuration
+            Connection connection = DriverManager.getConnection(DB_URL,DB_USERNAME, DB_PASSWORD);
+
+            //create sql query
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM users WHERE username = ? AND password = ?"
+            );
+
+            //replace the ? with values
+            //parameter index referring to the iteration of the ? so 1 is the first ? and 2 is the second ?
+
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            //execute query and store into a result set
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            //next() returns true or false
+            //true - query returned data and result set now points to the first row
+            //false = query returned no data nad result set equals to null
+
+            if (resultSet.next()) {
+                //success
+                //get id
+                int userId = resultSet.getInt("id");
+
+                //get current balance
+                BigDecimal current_balance = resultSet.getBigDecimal("current_balance");
+
+                //return user object
+                return new User(userId, username, password, current_balance);
+
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        //not valid user
+        return null;
+    }
+}
--- src/guis/BankingAppGui.java
+++ src/guis/BankingAppGui.java
@@ -30,5 +30,55 @@ public class BankingAppGui extends BaseFrame {
         welcomeMessageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(welcomeMessageLabel);
 
+        //create current balance label
+        JLabel currentBalanceLabel = new JLabel("Current Balance");
+        currentBalanceLabel.setBounds(0,80,getWidth()-10,30);
+        currentBalanceLabel.setFont(new Font("Dialog", Font.BOLD, 22));
+        currentBalanceLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(currentBalanceLabel);
+
+        //create current balance text field
+        JTextField currentBalanceField = new JTextField("MYR " + user.getCurrentBalance());
+        currentBalanceField.setBounds(15,120,getWidth()-50,40);
+        currentBalanceField.setFont(new Font("Dialog", Font.BOLD, 28));
+        currentBalanceField.setHorizontalAlignment(SwingConstants.RIGHT);
+        currentBalanceField.setEditable(false);
+        add(currentBalanceField);
+
+        //deposit button
+        JButton depositButton = new JButton("Deposit");
+        depositButton.setBounds(15,180,getWidth()-50,50);
+        depositButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        depositButton.setHorizontalAlignment(SwingConstants.CENTER);
+        add(depositButton);
+
+        //withdraw button
+        JButton withdrawButton = new JButton("Withdraw");
+        withdrawButton.setBounds(15,250,getWidth()-50,50);
+        withdrawButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        withdrawButton.setHorizontalAlignment(SwingConstants.CENTER);
+        add(withdrawButton);
+
+        //past transaction button
+        JButton pastTransactionButton = new JButton("Past Transaction");
+        pastTransactionButton.setBounds(15,320,getWidth()-50,50);
+        pastTransactionButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        pastTransactionButton.setHorizontalAlignment(SwingConstants.CENTER);
+        add(pastTransactionButton);
+
+        //transfer button
+        JButton transferButton = new JButton("Transfer");
+        transferButton.setBounds(15,390,getWidth()-50,50);
+        transferButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        transferButton.setHorizontalAlignment(SwingConstants.CENTER);
+        add(transferButton);
+
+        //logout button
+        JButton logoutButton = new JButton("Log Out");
+        logoutButton.setBounds(15,500,getWidth()-50,50);
+        logoutButton.setFont(new Font("Dialog", Font.BOLD, 22));
+        logoutButton.setHorizontalAlignment(SwingConstants.CENTER);
+        add(logoutButton);
+
     }
 }
